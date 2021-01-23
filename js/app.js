0ec1d8bb7fb1c11aff7060030a1512bb804ca957
Ryan Gallaway <rkgallaway@msn.com>
2021-01-22 23:29:34 -0800

    201n21 lab-05a setup

--- js/app.js
+++ js/app.js
@@ -9,11 +9,7 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-  var mySum = a + b;
-  var message = `The sum of ${a} and ${b} is ${mySum}.`;
-  // console.log([mySum, message]);
 
-  return [mySum, message];
 }
 
 // Here is the test for sum(); uncomment it to run it
@@ -31,7 +27,7 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-  
+
 }
 
 // Here is the test for multiply(); uncomment it to run it
