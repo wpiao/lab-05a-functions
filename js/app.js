d86036074276e5e854533b55385d11e0f2efd5ea
Wenhao Piao <wenhaopiao520@gmail.com>
2021-01-23 15:47:44 -0800

    Finished problem-four

--- js/app.js
+++ js/app.js
@@ -81,12 +81,25 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+  let message = '';
+  let sums = 0;
+  for (let i = 0; i < sumArr.length; i++) {
+    let currentNumber = sumArr[i];
+    sums = sum(sums, currentNumber)[0];
+    if (i !== sumArr.length - 1) { // if current number is not last element in the array
+      message += `${currentNumber},`;
+    } else { // if current number is last element in the array
+      message += `${currentNumber} `;
+    }
+  }
+  message += `was passed in as an array of numbers, and ${sums} is their sum.`;
+  // console.log([sums, message]);
+  return [sums, message];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray); //eslint-disable-line
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
