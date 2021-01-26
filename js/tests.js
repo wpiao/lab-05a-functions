a1cd924eab7f623ff3fbfccd25e87cd9e6ad27b9
Wenhao Piao <wenhaopiao520@gmail.com>
2021-01-25 19:38:46 -0800

    Fixed lint issue in the test file

--- js/tests.js
+++ js/tests.js
@@ -1,9 +1,10 @@
+/* eslint-disable no-undef */
 'use strict';
 
 // You should not modify anything in this file, but feel free to look through it!
 
 function testSum() { //eslint-disable-line
-  if (sum(4,7)[0] === 11 && sum(4,7)[1] === 'The sum of 4 and 7 is 11.') {
+  if (sum(4, 7)[0] === 11 && sum(4, 7)[1] === 'The sum of 4 and 7 is 11.') {
     console.log('%c TEST FOR sum() PASSES', 'color: green');
   } else {
     console.log('%c TEST FOR sum() FAILS', 'color: red');
@@ -11,7 +12,7 @@ function testSum() { //eslint-disable-line
 }
 
 function testMultiply() { //eslint-disable-line
-  if (multiply(5,9)[0] === 45 && multiply(5,9)[1] === 'The product of 5 and 9 is 45.') {
+  if (multiply(5, 9)[0] === 45 && multiply(5, 9)[1] === 'The product of 5 and 9 is 45.') {
     console.log('%c TEST FOR multiply() PASSES', 'color: green');
   } else {
     console.log('%c TEST FOR multiply() FAILS', 'color: red');
